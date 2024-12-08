951f32e2fada81b96332109f1ff5a6a6c735139c
yokheved <yokheved.spr@gmail.com>
2024-12-08 17:00:38 +0200

    Enhance User and UserCalender classes with password validation and cycle management

--- src/data/user.js
+++ src/data/user.js
@@ -1,8 +1,23 @@
+import UserCalender from "./userCalender";
 export default class User {
     constructor(params) {
         this.name = params.name;
         this.email = params.email;
-        this.password = params.password;
-        this.calender;
+        this._password = params.password;
+        this.calender = new UserCalender(params.email);
+    }
+
+    set password({value, confirm}) {
+        if(value !== confirm) {
+            throw new Error("Passwords do not match");
+        }
+        if(value.length < 4) {
+            throw new Error("Password must be at least 4 characters long");
+        }
+        this._password = value;
+    }
+
+    checkPassword(password) {
+        return this._password === password;
     }
 }
--- src/data/userCalender.js
+++ src/data/userCalender.js
@@ -1,6 +1,12 @@
+import MonthlyCycle from "./monthlyCycle";
 export default class UserCalender {
     constructor(email) {
         this.user = email;
-        this.
+        this.cycles = [];      
+    }
+
+    addCycle(firstBld) {
+        this.cycles.push(new MonthlyCycle(firstBld, 5));//get user and the amount of days counted
+        this.cycles.sort((a, b) => a.firstBld - b.firstBld);
     }
 }
